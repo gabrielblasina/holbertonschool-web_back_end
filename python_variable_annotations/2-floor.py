cb44576a29fb33ddaf7c8e6d2a93959944104fa8
gabrielblasina <5722@holbertonstudents.com>
2023-08-30 02:52:20 -0700

    task-2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module Variable_anotations"""
+
+
+def floor(n: float) -> int:
+    """
+    Args:
+        n (float): only paramether.
+    Returns:
+        floor of n.
+    """
+    return int(n)
