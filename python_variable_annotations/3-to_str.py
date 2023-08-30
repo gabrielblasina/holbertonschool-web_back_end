c77bc639c1b13d454fc8e1b643c89fa63ac8024f
gabrielblasina <5722@holbertonstudents.com>
2023-08-30 02:56:30 -0700

    task-3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module Variable_anotations"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n (float): only paramether.
+    Returns:
+        string representation of n.
+    """
+    return str(n)
