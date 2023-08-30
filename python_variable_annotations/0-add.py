0525cfa4fd9e41fa4601ce930075ec0925cef27d
gabrielblasina <5722@holbertonstudents.com>
2023-08-30 02:39:29 -0700

    task-0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module Variable_anotations"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args:
+        a (float): first number.
+        b (float): second number.
+    Returns:
+        sum of two numbers.
+    """
+    return a + b
