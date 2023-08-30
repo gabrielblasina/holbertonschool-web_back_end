26bbc40e83db64051d241b9c5d75f70b67202e87
gabrielblasina <5722@holbertonstudents.com>
2023-08-30 02:44:36 -0700

    task-1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module Variable_anotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Args:
+        str1 (str): first string.
+        str2 (str): second string.
+    Returns:
+        concatenated string.
+    """
+    return str1 + str2
