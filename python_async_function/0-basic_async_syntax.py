3fab1fc3acbe9ad95720dbbadeab2e98e7351302
gabrielblasina <5722@holbertonstudents.com>
2023-09-01 07:11:56 -0700

    task-0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""python_async_function"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Args:
+        max_delay: max wait
+
+    Return:
+        float time random
+    """
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
