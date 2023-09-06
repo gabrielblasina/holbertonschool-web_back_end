77a670941fdff56e3c4cf44243207e9a0fef69cf
gabrielblasina <5722@holbertonstudents.com>
2023-09-06 06:53:33 -0700

    task-11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""insert new document in a collection"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns the list of school having a specific topic
+    Args:
+        mongo_collection: collection object
+        topics ([str]): list with topic names
+    Return:
+        list of schools
+    """
+    schools_list = []
+    for school in mongo_collection.find({'topic': topic}):
+        schools_list.append(school)
+    return schools_list
